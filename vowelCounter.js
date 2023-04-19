4be30cca760b2764c6601d8458f64e6913f4eb3f
juanpardo123 <129297821+juanpardo123@users.noreply.github.com>
2023-04-19 15:20:20 -0400

    Add files via upload

--- vowelCounter.js
+++ vowelCounter.js
@@ -3,5 +3,5 @@ let message = "hello my friend";
 console.log(vowelCounter(message));
 
 function vowelCounter(word){
-    return message.match(/[aeiou]/gi).length;
+    return word.match(/[aeiou]/gi).length;
 }
